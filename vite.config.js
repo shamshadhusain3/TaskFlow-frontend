f74f1564c7bb1ccc23da76472e0cd0173e89a994
shamshadhusain3 <111483127+shamshadhusain3@users.noreply.github.com>
2024-09-30 16:01:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   },
    build: {
     rollupOptions: {
-      external: ['react-s-alert']
+      external: ['react-s-alert/dist/s-alert-default.css']
     }
   },
   plugins: [react()],
